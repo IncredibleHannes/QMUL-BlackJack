c7ca9623f54a9a55fafeffc232060417b17e95e3
Johannes Hartmann <johannes.Hartmann.Calw@web.de>
2018-02-15 19:09:28 +0000

    added some sort of sceleton

---
+++ Makefile
@@ -0,0 +1,21 @@
+CPPFLAGS = -std=c++11 -Wall -Werror -g -O0
+
+BIN = build
+SRC = $(shell find src/ -name *.cpp)
+OBJ = $(SRC:%.cpp=build/%.o)
+
+INCLUDE = -Isrc/Model -Isrc/Controller -Isrc/View
+
+APP = main
+
+all: $(APP)
+
+$(APP): $(OBJ)
+	g++ $(CPPFLAGS) $^ -o $(BIN)/$(APP)
+
+build/%.o: %.cpp
+	mkdir -p $(dir $@)
+	g++ $(CPPFLAGS) $(INCLUDE) -c $< -o $@
+
+clean:
+	rm -rf $(BIN)
---
+++ src/Controller/Controller.h
@@ -0,0 +1,23 @@
+/**
+    <h1>Controller</h1>
+    <h3>Controller.cpp</h3><br>
+    This class is representing the controller. The whole game is implemented
+    in the Model View Controller style. The run methode implements the whole game cicle.
+
+
+    @author Johannes Hartmann
+    @version 1.0
+*/
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "View.h"
+
+class Controller {
+public:
+  /**
+   * Starts the game and implements the whole game cicle
+   */
+  void run();
+};
+#endif
